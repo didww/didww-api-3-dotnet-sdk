2c885f0ac99ade87742044d8eee47f7656bbc639
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-03-10 23:34:39 +0100

    Add export download and decompress tests

--- tests/Didww.Api3.Tests/ExportTest.cs
+++ tests/Didww.Api3.Tests/ExportTest.cs
@@ -1,6 +1,10 @@
+using System.IO.Compression;
+using Didww.Api3.Exception;
 using Didww.Api3.Resource;
 using Didww.Api3.Resource.Enums;
 using FluentAssertions;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
 
 namespace Didww.Api3.Tests;
 
@@ -80,5 +84,106 @@ public class ExportTest : BaseTest
         created.Id.Should().Be("da15f006-5da4-45ca-b0df-735baeadf423");
         created.ExportType.Should().Be(ExportType.CdrOut);
         created.Status.Should().Be(ExportStatus.Pending);
+    }
+
+    [Fact]
+    public async Task TestDownloadExport()
+    {
+        var csvContent = "Date/Time Start (UTC),DID,Duration\n2018-12-06,972397239159652,0\n";
+        var gzData = CompressToGzip(csvContent);
+
+        var exportUrl = WireMock.Url + "/v3/exports/02bf6df4-3af9-416c-96be-16e5b7eeb651.csv.gz";
+        WireMock.Given(
+            Request.Create().WithPath("/v3/exports/02bf6df4-3af9-416c-96be-16e5b7eeb651.csv.gz").UsingGet()
+        ).RespondWith(
+            Response.Create()
+                .WithStatusCode(200)
+                .WithHeader("Content-Type", "application/octet-stream")
+                .WithHeader("Content-Disposition", "attachment; filename=\"02bf6df4-3af9-416c-96be-16e5b7eeb651.csv.gz\"")
+                .WithBody(gzData)
+        );
+
+        var export = new Export { Id = "02bf6df4-3af9-416c-96be-16e5b7eeb651", Url = exportUrl };
+        var tempFile = Path.GetTempFileName();
+        try
+        {
+            await Client.DownloadExportAsync(export, tempFile);
+            var bytes = await File.ReadAllBytesAsync(tempFile);
+            // Verify gzip magic bytes
+            bytes[0].Should().Be(0x1f);
+            bytes[1].Should().Be(0x8b);
+        }
+        finally
+        {
+            File.Delete(tempFile);
+        }
+    }
+
+    [Fact]
+    public async Task TestDownloadAndDecompressExport()
+    {
+        var csvContent = "Date/Time Start (UTC),DID,Duration\n2018-12-06,972397239159652,0\n";
+        var gzData = CompressToGzip(csvContent);
+
+        var exportUrl = WireMock.Url + "/v3/exports/02bf6df4-3af9-416c-96be-16e5b7eeb651.csv.gz";
+        WireMock.Given(
+            Request.Create().WithPath("/v3/exports/02bf6df4-3af9-416c-96be-16e5b7eeb651.csv.gz").UsingGet()
+        ).RespondWith(
+            Response.Create()
+                .WithStatusCode(200)
+                .WithHeader("Content-Type", "application/octet-stream")
+                .WithHeader("Content-Disposition", "attachment; filename=\"02bf6df4-3af9-416c-96be-16e5b7eeb651.csv.gz\"")
+                .WithBody(gzData)
+        );
+
+        var export = new Export { Id = "02bf6df4-3af9-416c-96be-16e5b7eeb651", Url = exportUrl };
+        var tempFile = Path.GetTempFileName();
+        try
+        {
+            await Client.DownloadAndDecompressExportAsync(export, tempFile);
+            var content = await File.ReadAllTextAsync(tempFile);
+            content.Should().Contain("Date/Time Start (UTC)");
+            content.Should().Contain("972397239159652");
+        }
+        finally
+        {
+            File.Delete(tempFile);
+        }
+    }
+
+    [Fact]
+    public async Task TestDownloadExportNullUrl()
+    {
+        var export = new Export { Id = "test-id", Url = null };
+
+        var act = () => Client.DownloadExportAsync(export, "/tmp/test.csv");
+        await act.Should().ThrowAsync<DidwwClientException>()
+            .WithMessage("*URL is null*");
+    }
+
+    [Fact]
+    public async Task TestDownloadExportHttpError()
+    {
+        var exportUrl = WireMock.Url + "/v3/exports/nonexistent.csv.gz";
+        WireMock.Given(
+            Request.Create().WithPath("/v3/exports/nonexistent.csv.gz").UsingGet()
+        ).RespondWith(
+            Response.Create().WithStatusCode(404)
+        );
+
+        var export = new Export { Id = "nonexistent", Url = exportUrl };
+        var act = () => Client.DownloadExportAsync(export, "/tmp/test.csv");
+        await act.Should().ThrowAsync<DidwwClientException>()
+            .WithMessage("*404*");
+    }
+
+    private static byte[] CompressToGzip(string content)
+    {
+        using var ms = new MemoryStream();
+        using (var gz = new GZipStream(ms, CompressionMode.Compress, leaveOpen: true))
+        {
+            gz.Write(System.Text.Encoding.UTF8.GetBytes(content));
+        }
+        return ms.ToArray();
     }
 }
